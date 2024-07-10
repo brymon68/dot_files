cb9b6973fa4d0826848ffb9a17fb75450ba19702
bryce.montano <bryce.montano@workdayinternal.com>
2024-07-10 11:46:00 -0600

    updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
     { "williamboman/mason.nvim", config = true },
     "williamboman/mason-lspconfig.nvim",
     { "j-hui/fidget.nvim", opts = {} },
-    "folke/neodev.nvim",
     { "b0o/schemastore.nvim" },
     { "hrsh7th/cmp-nvim-lsp" },
   },
@@ -24,8 +23,6 @@ return {
       ensure_installed = vim.tbl_keys(require("plugins.lsp.servers")),
     })
     require("lspconfig.ui.windows").default_options.border = "single"
-
-    require("neodev").setup()
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
