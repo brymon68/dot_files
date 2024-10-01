d9ea6321fa7c00feddc001dbbef749ab74f9e412
Bryce Montano <brycemontano1@gmail.com>
2024-09-30 19:30:51 -0600

    Uhhhhh

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- disable trouble
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-}
