43e36d785966830615a750c83b70390c28d02a57
Bryce Montano <brycemontano1@gmail.com>
2025-01-31 19:35:26 -0700

    Migration to tokyo

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,37 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
+	dependencies = {
+		{
+			"f-person/auto-dark-mode.nvim",
+			opts = {
+				update_interval = 1000,
+				set_dark_mode = function()
+					vim.api.nvim_set_option_value("background", "dark", {})
+					vim.cmd("colorscheme tokyonight-night")
+				end,
+				set_light_mode = function()
+					vim.api.nvim_set_option_value("background", "light", {})
+					vim.cmd("colorscheme tokyonight-day")
+				end,
+			},
+		},
+	},
+	background = {
+		light = "day",
+		dark = "night",
+	},
 	config = function()
-		require("tokyonight").setup({
-			style = "night",
-		})
-		vim.cmd([[colorscheme tokyonight-night]])
+		require("auto-dark-mode").init()
+
+		vim.cmd.colorscheme("tokyonight-night")
+
+		vim.api.nvim_create_user_command("DD", function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end, {})
+
+		vim.api.nvim_create_user_command("DL", function()
+			vim.cmd.colorscheme("tokyonight-day")
+		end, {})
 	end,
 }
