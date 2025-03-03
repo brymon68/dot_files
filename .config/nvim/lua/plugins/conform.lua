0628b67ae8f27db8b42e21d6419ff55cffa2012b
brymon68 <bryce.montano1@gmail.com>
2025-03-03 12:49:07 -0700

    Updating brewfile, adding borders, updating delay on conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,31 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufWritePre"},
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufWritePre" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
+    conform.setup({
       log_level = vim.log.levels.DEBUG,
       notify_on_error = true,
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				go = { "gofmt" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				lua = { "stylua" },
-				python = { "black" },
-			},
-			format_on_save = {
-				lsp_format = "fallback",
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-	end,
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        go = { "gofmt" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = { "black" },
+      },
+      format_on_save = {
+        lsp_format = "fallback",
+        async = false,
+        timeout_ms = 3000,
+      },
+    })
+  end,
 }
