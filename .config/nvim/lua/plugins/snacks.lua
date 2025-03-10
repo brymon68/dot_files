cde9249897081b052488ed23bf4e571647f5dcfe
brymon68 <brycemontano1@gmail.com>
2025-03-09 20:17:03 -0600

    Adding yazi, tweaks to snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,20 +4,42 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
-    explorer = { enabled = true },
+    explorer = {
+      enabled = true,
+    },
     quickfile = { enabled = true },
     scroll = { enabled = true },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        }
+      }
+    }
   },
   keys = {
     { "<leader>ff", function() Snacks.picker.files() end,   desc = "Find Files" },
     { "<leader>fs", function() Snacks.picker.grep() end,    desc = "Grep" },
     { "<leader>fh", function() Snacks.picker.help() end,    desc = "Help Pages" },
     { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
-    { "<leader>e",  function() Snacks.explorer() end,       desc = "File Explorer" },
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer({
+          git_status = true,
+          hidden     = true,
+          layout     = {
+            position = "right",
+          }
+        })
+      end,
+      desc = "File Explorer"
+    },
   }
 
 }
--- .zshrc
+++ .zshrc
@@ -6,6 +6,7 @@ export PATH=$HOME/.cargo/bin:$PATH
 export PATH=/opt/homebrew/bin:$PATH
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.tmuxifier/bin:$PATH"
+export EDITOR="nvim"
 
 # PROMPT
 export CLICOLOR=1
@@ -32,3 +33,24 @@ alias cat="bat"
 
 export HOMEBREW_BREWFILE=~/.config/Brewfile.txt
 export EZA_CONFIG_DIR=~/.config/eza
+
+# pnpm
+export PNPM_HOME="/Users/gondor/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/gondor/.lmstudio/bin"
+
+#yazi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
