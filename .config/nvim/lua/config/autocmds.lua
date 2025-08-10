15b5e16355f3e976a1219964f0a2f8e94bb9187a
brymon68 <bryce.montano1@gmail.com>
2025-08-10 12:49:57 -0600

    Adding zenmode, other helpful autocommands

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -33,3 +33,93 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end, { noremap = true, silent = true, buffer = event.buf })
 	end,
 })
+
+-- Toggle linting
+local linting_enabled = true
+local map = vim.keymap.set
+map("n", "<leader>lt", function()
+	linting_enabled = not linting_enabled
+	if linting_enabled then
+		-- Re-enable linting by recreating the autocmds
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				local lint = require("lint")
+				local linters = lint.linters_by_ft[vim.bo.filetype]
+				if linters and #linters > 0 then
+					lint.try_lint()
+				end
+			end,
+		})
+		vim.notify("Linting enabled", vim.log.levels.INFO, { title = "nvim-lint" })
+	else
+		-- Disable linting by clearing the augroup and diagnostics
+		vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.diagnostic.reset()
+		vim.notify("Linting disabled", vim.log.levels.INFO, { title = "nvim-lint" })
+	end
+end)
+
+-- Toggle windline
+local windline_enabled = true
+map("n", "<leader>wt", function()
+	windline_enabled = not windline_enabled
+	if windline_enabled then
+		vim.o.laststatus = 2
+		vim.notify("Windline enabled", vim.log.levels.INFO, { title = "Windline" })
+	else
+		vim.o.laststatus = 0
+		vim.notify("Windline disabled", vim.log.levels.INFO, { title = "Windline" })
+	end
+end, { desc = "Toggle windline" })
+
+-- Toggle zen mode - disable linting, windline, bufferline, and copilot
+local zen_mode_enabled = false
+map("n", "<leader>z", function()
+	zen_mode_enabled = not zen_mode_enabled
+	if zen_mode_enabled then
+		-- Disable linting
+		linting_enabled = false
+		vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.diagnostic.reset()
+
+		-- Disable windline
+		windline_enabled = false
+		vim.o.laststatus = 0
+
+		-- Disable bufferline
+		vim.o.showtabline = 0
+
+		-- Disable copilot
+		vim.cmd("Copilot disable")
+
+		vim.notify("Zen mode enabled", vim.log.levels.INFO, { title = "Zen Mode" })
+	else
+		-- Re-enable linting
+		linting_enabled = true
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				local lint = require("lint")
+				local linters = lint.linters_by_ft[vim.bo.filetype]
+				if linters and #linters > 0 then
+					lint.try_lint()
+				end
+			end,
+		})
+
+		-- Re-enable windline
+		windline_enabled = true
+		vim.o.laststatus = 2
+
+		-- Re-enable bufferline
+		vim.o.showtabline = 2
+
+		-- Re-enable copilot
+		vim.cmd("Copilot enable")
+
+		vim.notify("Zen mode disabled", vim.log.levels.INFO, { title = "Zen Mode" })
+	end
+end, { desc = "Toggle zen mode" })
