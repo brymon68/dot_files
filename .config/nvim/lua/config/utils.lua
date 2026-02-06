6a81fe3e7f0edeef2c9f077da881c9244a3c0a44
brymon68 <bryce.montano1@gmail.com>
2026-02-06 11:54:53 -0700

    Leader lg for github stuff, updating lazy

--- .config/nvim/lua/config/utils.lua
+++ .config/nvim/lua/config/utils.lua
@@ -43,9 +43,16 @@ M.snacks_git_or_file = function()
 	end
 end
 
-M.copyFilePathAndLineNumber = function()
+M.copyFilePathAndLineNumber = function(visual)
 	local current_file = vim.fn.expand("%:p")
-	local current_line = vim.fn.line(".")
+	local line_ref
+	if visual then
+		local start_line = vim.fn.line("'<")
+		local end_line = vim.fn.line("'>")
+		line_ref = string.format("L%s-L%s", start_line, end_line)
+	else
+		line_ref = "L" .. vim.fn.line(".")
+	end
 	local is_git_repo = vim.fn.system("git rev-parse --is-inside-work-tree"):match("true")
 
 	if is_git_repo then
@@ -53,7 +60,7 @@ M.copyFilePathAndLineNumber = function()
 		local current_branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
 
 		-- Convert Git URL to GitHub web URL format
-		current_repo = current_repo:gsub("git@github.com:", "https://github.com/")
+		current_repo = current_repo:gsub("git@([^:]+):", "https://%1/")
 		current_repo = current_repo:gsub("%.git$", "")
 
 		-- Remove leading system path to repository root
@@ -62,13 +69,13 @@ M.copyFilePathAndLineNumber = function()
 			current_file = current_file:sub(#repo_root + 2)
 		end
 
-		local url = string.format("%s/blob/%s/%s#L%s", current_repo, current_branch, current_file, current_line)
+		local url = string.format("%s/blob/%s/%s#%s", current_repo, current_branch, current_file, line_ref)
 		vim.fn.setreg("+", url)
 		print("Copied to clipboard: " .. url)
 	else
 		-- If not in a Git directory, copy the full file path
-		vim.fn.setreg("+", current_file .. "#L" .. current_line)
-		print("Copied full path to clipboard: " .. current_file .. "#L" .. current_line)
+		vim.fn.setreg("+", current_file .. "#" .. line_ref)
+		print("Copied full path to clipboard: " .. current_file .. "#" .. line_ref)
 	end
 end
 
